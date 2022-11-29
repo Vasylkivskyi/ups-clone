9fac737076ba2e7d38d79a49d3ecc8117d5decdf
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2022-11-29 22:49:02 +0200

    add basic order screen

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -1,46 +1,45 @@
-import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
-import { Card, Icon } from "@rneui/themed";
-import { useTailwind } from "tailwind-rn/dist";
+import React from 'react'
+import { View, Text, TouchableOpacity } from 'react-native'
+import { Card, Icon } from '@rneui/themed'
+import { useTailwind } from 'tailwind-rn/dist'
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native'
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs'
+import { TabStackParamList } from '../navigator/TabNavigator'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../navigator/RootNavigator'
+import { OrdersScreenNavigationProp } from '../screens/OrdersScreen'
 
 type Props = {
-  item: Order;
-};
+  item: Order
+}
 
 const OrderCard: React.FC<Props> = ({ item }) => {
-  const tw = useTailwind();
+  const tw = useTailwind()
+  const navigation = useNavigation<OrdersScreenNavigationProp>()
   return (
-    <TouchableOpacity>
-      <Card containerStyle={tw("px-5 rounded-lg")}>
-        <View style={tw("flex-row justify-between items-center")}>
+    <TouchableOpacity onPress={() => navigation.navigate('Order', { order: item })}>
+      <Card containerStyle={tw('px-5 rounded-lg')}>
+        <View style={tw('flex-row justify-between items-center')}>
           <View>
-            <Icon
-              name="truck-delivery"
-              color="#EB6A7C"
-              type="material-community"
-            />
-            <Text style={{ fontSize: 10 }}>
-              {new Date(item.createdAt).toDateString()}
-            </Text>
+            <Icon name="truck-delivery" color="#EB6A7C" type="material-community" />
+            <Text style={{ fontSize: 10 }}>{new Date(item.createdAt).toDateString()}</Text>
           </View>
-          <View style={tw("")}>
-            <Text style={[tw("text-gray-400"), { fontSize: 10 }]}>
+          <View style={tw('')}>
+            <Text style={[tw('text-gray-400'), { fontSize: 10 }]}>
               {item.carrier}-{item.trackingId}
             </Text>
-            <Text style={tw("text-gray-500 text-xl")}>
-              {item.trackingItems.customer.name}
-            </Text>
+            <Text style={tw('text-gray-500 text-xl')}>{item.trackingItems.customer.name}</Text>
           </View>
-          <View style={tw("flex-row items-center")}>
-            <Text style={[tw("text-sm"), { color: "#EB6A7C" }]}>
+          <View style={tw('flex-row items-center')}>
+            <Text style={[tw('text-sm'), { color: '#EB6A7C' }]}>
               {item.trackingItems.items.length}x
             </Text>
-            <Icon style={tw("ml-2")} name="box" type="feather" />
+            <Icon style={tw('ml-2')} name="box" type="feather" />
           </View>
         </View>
       </Card>
     </TouchableOpacity>
-  );
-};
+  )
+}
 
-export default OrderCard;
+export default OrderCard
---
+++ src/screens/OrderScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class OrderScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>OrderScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default OrderScreen
