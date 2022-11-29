8b257f0e397c0c4e136d8558d93b936704d12db5
Vasylkivskyi <pavel.vasylkivskiy@gmail.com>
2022-11-29 23:41:07 +0200

    add order card

--- src/screens/OrderScreen.tsx
+++ src/screens/OrderScreen.tsx
@@ -1,14 +1,32 @@
 import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import React, { useLayoutEffect } from 'react'
+import { useTailwind } from 'tailwind-rn/dist'
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native'
+import { OrdersScreenNavigationProp } from './OrdersScreen'
+import { RootStackParamList } from '../navigator/RootNavigator'
+import DeliveryCard from '../components/DeliveryCard'
 
-export class OrderScreen extends Component {
-  render() {
-    return (
-      <View>
-        <Text>OrderScreen</Text>
-      </View>
-    )
-  }
+type OrderScreenRouteProp = RouteProp<RootStackParamList, 'Order'>
+
+export const OrderScreen = () => {
+  const tw = useTailwind()
+  const navigation = useNavigation<OrdersScreenNavigationProp>()
+  const {
+    params: { order },
+  } = useRoute<OrderScreenRouteProp>()
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: order.trackingItems.customer.name,
+      headerTintColor: '#EB6A7C',
+      headerBackTitle: 'Deliveries',
+    })
+  }, [order])
+
+  return (
+    <View style={tw('-mt-2')}>
+      <DeliveryCard order={order} fullWidth />
+    </View>
+  )
 }
 
 export default OrderScreen
